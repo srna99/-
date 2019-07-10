db8bfd97f0c4b996f8ffd5d01522605f05c316dd
Serena Cheng <serena.ch1999@gmail.com>
2019-07-10 14:02:47 -0400

    Add completed storyboard and typing numbers capability.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    var isFinishedTyping = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func functionPressed(_ sender: UIButton) {
+        
+        isFinishedTyping = true
+        
+    }
+    
+    @IBAction func numberPressed(_ sender: UIButton) {
+        
+        if let number = sender.currentTitle {
+            
+            if isFinishedTyping {
+                numberLabel.text = number
+                isFinishedTyping = false
+            }
+            else {
+                numberLabel.text = numberLabel.text! + number
+            }
+            
+        }
+        
+    }
 }
 
